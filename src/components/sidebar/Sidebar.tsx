260bf00d12b18f2d340129044affc547fb9f65db
Anant Tripathi <anant23.tripathi@gmail.com>
2024-07-05 18:21:54 +0530

    sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,15 +1,32 @@
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './sidebar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUpload } from '@fortawesome/free-solid-svg-icons';
+import { faUpload, faRemove } from '@fortawesome/free-solid-svg-icons';
 
 const Sidebar = () => {
 
     const fileInputRef = useRef<HTMLInputElement>(null);
+    const videoRef = useRef<HTMLVideoElement>(null);
+    const [videoURL, setVideoURL] = useState<string>('');
 
     const handleClick = () => {
         if (fileInputRef.current) fileInputRef.current.click();
     };
+
+    const handleFileChange = (event: any) => {
+        const file = event.target.files[0];
+        if (file) {
+          const url = URL.createObjectURL(file);
+          setVideoURL(url);
+        }
+    };
+
+    useEffect(() => {
+        if (videoURL && videoRef.current) {
+            videoRef.current.volume = 0.1;
+        }
+        console.log(videoURL);
+    }, [videoURL]);
     
   return (
     <div className='sidebar'>
@@ -17,14 +34,21 @@ const Sidebar = () => {
             <h1>WishVA</h1>
         </div>
         <div className="sidebar-container">
-            <div className="upload-container">
-                <div className="upload-box" onClick={handleClick}>
-                    <input type="file" ref={fileInputRef} style={{display: 'none'}} accept="video/*" />
+            {videoURL ? (
+                <div className="video-preview">
+                    <video ref={videoRef} controls src={videoURL} autoPlay className='video'/>
+                </div>
+                ) : (
+                <div className="upload-container">
+                    <div className="upload-box" onClick={handleClick}>
+                    <input type="file" ref={fileInputRef} style={{display: 'none'}} accept="video/*" onChange={handleFileChange} />
                     <FontAwesomeIcon icon={faUpload} />
                     <p>Click here to upload a file</p>
+                    </div>
                 </div>
-            </div>
+            )}
             <div className="button-container">
+                <button className='remove-button' onClick={() => setVideoURL('')}><FontAwesomeIcon icon={faRemove} /></button>
                 <button className="upload-button">Upload</button>
             </div>
             <div className="transcript">
